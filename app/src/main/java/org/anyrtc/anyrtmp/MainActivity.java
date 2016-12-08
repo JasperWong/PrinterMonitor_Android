9637f5c58ee7502df0312c8f2596823c18570a5d
JasperWong <766453486@qq.com>
2016-12-09 01:26:43 +0800

    function done,but with logical problem

--- app/src/main/java/org/anyrtc/anyrtmp/MainActivity.java
+++ app/src/main/java/org/anyrtc/anyrtmp/MainActivity.java
@@ -19,6 +19,9 @@
 package org.anyrtc.anyrtmp;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -26,6 +29,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -34,30 +38,58 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import org.anyrtc.core.AnyRTMP;
 import org.anyrtc.core.RTMPGuestHelper;
 import org.anyrtc.core.RTMPGuestKit;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.webrtc.SurfaceViewRenderer;
 import org.webrtc.VideoRenderer;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,RTMPGuestHelper {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, RTMPGuestHelper {
 
     private EditText mEditRtmpUrl;
     private SurfaceViewRenderer mSurfaceView = null;
     private VideoRenderer mRenderer = null;
-    private String rtmpUrl="rtmp://123.207.18.69:1935/myapp/testav";
+    private String rtmpUrl = "rtmp://123.207.18.69:1935/myapp/testav";
     private RTMPGuestKit mGuest = null;
-    private ImageView mSwitch1=null;
-    private ImageView mSwitch2=null;
-    private ImageView mCamera=null;
-    private int IsSwitch2On=0;
-    private int IsCameraOn=0;
-    private int IsSwitch1On=0;
+    private ImageView mSwitch1 = null;
+    private ImageView mSwitch2 = null;
+    private ImageView mCamera = null;
+    private int IsSwitch2On ;
+    private int IsCameraOn;
+    private int IsSwitch1On;
 
-    Timer UpdateTimer =new Timer();
+    private int switch1Remote;
+    private int switch2Remote;
+    private int cameraRemote;
+
+    private boolean isChange = false;
+    public static final int RESPONSE = 0;
+
+    final private int SelectID = 1;
+    Timer UpdateTimer = new Timer();
+
+    private String remoteJson = null;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,75 +112,147 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mSurfaceView = (SurfaceViewRenderer) findViewById(R.id.surface_view);
         mSurfaceView.init(AnyRTMP.Inst().Egl().getEglBaseContext(), null);
         mRenderer = new VideoRenderer(mSurfaceView);
-        mSwitch1=(ImageView)findViewById(R.id.switch1_iv);
-        mSwitch2=(ImageView)findViewById(R.id.switch2_iv);
-        mCamera=(ImageView)findViewById(R.id.camera_iv);
+        mSwitch1 = (ImageView) findViewById(R.id.switch1_iv);
+        mSwitch2 = (ImageView) findViewById(R.id.switch2_iv);
+        mCamera = (ImageView) findViewById(R.id.camera_iv);
 
         mSwitch1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(IsSwitch1On==0){
-                    IsSwitch1On=1;
+                if (IsSwitch1On == 0) {
+                    IsSwitch1On = 1;
                     mSwitch1.setImageResource(R.drawable.switch_on);
-                }else if(IsSwitch1On==1){
-                    IsSwitch1On=0;
+                } else if (IsSwitch1On == 1) {
+                    IsSwitch1On = 0;
                     mSwitch1.setImageResource(R.drawable.switch_off);
                 }
+                isChange = true;
             }
         });
         mSwitch2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(IsSwitch2On==0){
-                    IsSwitch2On=1;
+                if (UpdateTimer != null) {
+                    UpdateTimer.cancel();
+                    UpdateTimer = null;
+                }
+                if (IsSwitch2On == 0) {
+                    IsSwitch2On = 1;
                     mSwitch2.setImageResource(R.drawable.switch_on);
-                }else if(IsSwitch2On==1){
-                    IsSwitch2On=0;
+                } else if (IsSwitch2On == 1) {
+                    IsSwitch2On = 0;
                     mSwitch2.setImageResource(R.drawable.switch_off);
                 }
+                isChange = true;
             }
         });
         mCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(IsCameraOn==0){
+                if (IsCameraOn == 0) {
                     mGuest.StartRtmpPlay(rtmpUrl, mRenderer.GetRenderPointer());
-                    IsCameraOn=1;
+                    IsCameraOn = 1;
                     mCamera.setImageResource(R.drawable.switch_on);
-                }else if(IsCameraOn==1){
+                } else if (IsCameraOn == 1) {
                     mGuest.StopRtmpPlay();
-                    IsCameraOn=0;
+                    IsCameraOn = 0;
                     mCamera.setImageResource(R.drawable.switch_off);
                 }
+                isChange = true;
             }
         });
         AnyRTMP.Inst();
         mGuest = new RTMPGuestKit(this, this);
-        UpdateTimer.schedule(UpdateTask,1000,50);
+        UpdateTimer.schedule(UpdateTask,0, 50);
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void OnBtnClicked(View view) {
 
     }
 
+    private void makeCameraOn(){
+        mGuest.StartRtmpPlay(rtmpUrl, mRenderer.GetRenderPointer());
+        IsCameraOn = 1;
+        mCamera.setImageResource(R.drawable.switch_on);
+    }
 
+    private void makeSwitch1On(){
 
-    TimerTask UpdateTask=new TimerTask() {
+    }
+
+    TimerTask UpdateTask = new TimerTask() {
         @Override
         public void run() {
+            if (isChange) {
+                String url = new String(
+                        "http://123.207.18.69:8082/3dPrinterMonitor/user/insert?"
+                                + "id=" + SelectID
+                                + "&username=first"
+                                + "&switch1=" + IsSwitch1On
+                                + "&switch2=" + IsSwitch2On
+                                + "&camera=" + IsCameraOn
+                );
+                sendRequestWithHttpURLConnection(url);
+            } else if (!isChange) {
+                String url = new String("http://123.207.18.69:8082/3dPrinterMonitor/user/index?" + "SelectId=" + SelectID);
+                sendRequestWithHttpURLConnection(url);
+            }
 
+            if(switch2Remote==1){
+                makeCameraOn();
+            }
+            else if(switch2Remote==0){
 
+            }
 
         }
     };
 
 
+    private Handler handler = new Handler() {
+
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case RESPONSE: {
+                    String response = (String) msg.obj;
+
+                    String success = new String("update success");
+                    if (response.equals(success)) {
+                        isChange = false;
+                    } else {
+                        remoteJson = response;
+                        {
+                            try {
+                                JSONObject jsonObject = new JSONObject(remoteJson);
+                                switch2Remote=Integer.parseInt(jsonObject.getString("switch2"));
+                                switch1Remote=Integer.parseInt(jsonObject.getString("switch1"));
+                                cameraRemote=Integer.parseInt(jsonObject.getString("camera"));
+                                Log.d("switch2", switch2Remote + "");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        Log.d("test", remoteJson);
+
+                    }
+                    break;
+                }
+            }
+        }
+
+    };
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(UpdateTimer!=null){
+        if (UpdateTimer != null) {
             UpdateTimer.cancel();
-            UpdateTimer=null;
+            UpdateTimer = null;
         }
         if (mGuest != null) {
             mGuest.StopRtmpPlay();
@@ -195,12 +299,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
     @Override
     public void OnRtmplayerOK() {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(MainActivity.this,"打开监控",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "打开监控", Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -220,8 +325,83 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(MainActivity.this,"关闭监控",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "关闭监控", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private void sendRequestWithHttpURLConnection(final String urlConfig) {
+        // 开启线程来发起网络请求
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL(new String(urlConfig));
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    connection.setDoInput(true);
+                    connection.setDoOutput(true);
+                    InputStream in = connection.getInputStream();
+                    // 下面对获取到的输入流进行读取
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    Message message = new Message();
+                    message.what = RESPONSE;
+                    // 将服务器返回的结果存放到Message中
+                    message.obj = response.toString();
+                    handler.sendMessage(message);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
